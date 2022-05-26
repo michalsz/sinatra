cc4da19a3e8ad2dd947a415b445ccba5b0557671
Michal Szymanski <michal.t.szymanski@gmail.com>
2022-05-26 17:50:16 +0200

    Added html page

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'sinatra'
 require 'json'
 
+get '/' do
+  @page_title = 'Home'
+  @page_id = 'index.html'
+  erb :'index.html'
+end
+
 get '/hello/:name', :provides => :json do
   { greeting: "Hello #{params['name']}!" }.to_json
 end
